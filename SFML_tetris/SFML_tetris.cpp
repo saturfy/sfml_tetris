2ca79d5bf9e6bae51cec12075f4bfd9d61b1c0c5
Peti <saturfy@gmail.com>
2020-06-22 16:50:38 +0200

    added tetromino color and play area drawing

--- SFML_tetris/SFML_tetris.cpp
+++ SFML_tetris/SFML_tetris.cpp
@@ -7,7 +7,30 @@ const int M = 20;
 const int N = 10;
 
 //play area state hold in the field variable
-std::array < std::array<int, N>, M> field;
+std::array < std::array<int, N>, M> field = 
+
+{ {
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,1,0,0,0,0,0,0},
+} };
 
 // define the tetrominos
 std::array< std::array<int, 4>, 7> figures =
@@ -52,8 +75,9 @@ int coldet(std::array<Point, 4> a)
 
 int main()
 {	
-	//choose a tetromino from the list of 7 in figures
-	int n = 3;
+	// spawn tetromino
+	int n = 2; //choose a tetromino from the list of 7 in figures
+	int colornum = 2; // the number corresponding to the color of the tetromino
 	for (int i = 0; i < 4; i++)
 	{
 		a[i].x = figures[n][i] % 2;
@@ -72,21 +96,24 @@ int main()
 
 	// create drawable sprite from the texture
 	sf::Sprite s(t);
-	s.setTextureRect(sf::IntRect(0, 0, 18, 18));
+	
 
 	//variables for moving the tetromino
 	int dx = 0;
-	int colorNum = 1;
 
 	float timer = 0;
 	float delay = 0.1;
 
 	sf::Clock clock;
 	int counter = 0;
-	bool fall=true;
-	bool rotate = false;
+	// flags for game logic
+	bool fall=true; // true if the tetromino has to move down 1 step in this cycle
+	bool rotate = false; // true if the user pressed the rotate button and tetromina has to be rotated in this cycle
+	bool lock = false; // true if the tetromni touched something below it. It stops moving and "locked" 
 
 	int D = 10; // sets the falling speed (1 step fall per D update cycle)
+
+	
 
 	while (window.isOpen())
 	{
@@ -94,9 +121,9 @@ int main()
 		float time = clock.getElapsedTime().asSeconds();
 		clock.restart();
 		timer += time;
-		
 
-		//checking to close the window
+
+		//checking the user input
 		sf::Event e;
 		while (window.pollEvent(e))
 		{
@@ -112,25 +139,25 @@ int main()
 				else if (e.key.code == sf::Keyboard::Right) dx = 1;
 			}
 		}
-		
-		
+
+
 		//Movement and game logic is updated after the delay time 
 		if (timer > delay)
 		{
-			
+
 			// difficulty: this sets how many times the delay time passes before the tetromino falls 1 step
 			counter++;
-			if (counter == D) 
+			if (counter == D)
 			{
 				fall = true;
 				counter = 0;
 			}
 
-			
+
 			//Movement always happens in order
 			// First we take user input (move OR rotate) and check if its possible
 			// After user input we make the tetromino fall if fall flag is true
-			
+
 			//sideways movement 
 			if (dx != 0)
 			{
@@ -160,7 +187,7 @@ int main()
 			}
 
 			//Rotate
-			else if(rotate == true)
+			else if (rotate == true)
 			{
 				Point p = a[1]; //center of rotation
 				for (int i = 0; i < 4; i++)
@@ -175,7 +202,7 @@ int main()
 				}
 				//Reste flags
 				rotate = false;
-				
+
 				//collision detection 		
 				if (coldet(a) != 0)
 				{
@@ -187,8 +214,8 @@ int main()
 					}
 
 				}
-			
-			
+
+
 			}
 
 			// Fall
@@ -214,21 +241,38 @@ int main()
 						a[i].x = ta[i].x;
 						a[i].y = ta[i].y;
 					}
-
+					lock = true;
 				}
 			}
 
 
-			
+
 
 			//clear the window
 			window.clear(sf::Color::White);
 
 			//draw game elements
 
+			// draw field
+			for (int i = 0; i < M; i++)
+			{
+				for (int j = 0; j < N; j++)
+				{
+					// set the texture rect for the correct color
+					s.setTextureRect(sf::IntRect(field[i][j] * 18, 0, 18, 18));
+					s.setPosition(j * 18, i * 18);
+					window.draw(s);
+				}
+			}
+
 			//draw a tetromino
+			// set the texture rect for the correct color
+			s.setTextureRect(sf::IntRect( colornum * 18,0, 18, 18));
+
+			// draw tetromino pieces
 			for (int i = 0; i < 4; i++)
 			{
+				// set the texture position
 				s.setPosition(a[i].x * 18, a[i].y * 18);
 				window.draw(s);
 			}
